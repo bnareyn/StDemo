8b23d5ce30ce8d25e336112ed3b2d4c1a0197e2e
Binulal Narayanan <binulal@gmail.com>
2022-09-30 13:12:58 -0400

    added new pages

---
+++ pages/calgary.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+
+import plotly.express as px
+
+
+st.sidebar.header("Calgary Crime Data")
+
+@st.cache
+def get_data():
+    df = pd.read_csv(DATA_URL)
+    df[['lat', 'lon']] = df['Community Center Point'].str.split( ',', expand=True)
+    df[['x', 'lat']] = df.lat.str.split('(', expand = True)
+    df[['lon','y']] = df.lon.str.split(')', expand = True)
+    df.drop(['ID', 'Community Center Point', 'Date', 'x', 'y'], axis=1, inplace = True)
+    df.lat = df.lat.apply(float)
+    df.lon = df.lon.apply(float)
+    print(df.dtypes)
+    return df
+
+data = get_data()
+
+dfCrimeCount = data[['Sector', 'Year', 'Crime Count']].groupby(['Year', 'Sector']).sum().reset_index()
+
+fig = px.bar(dfCrimeCount, x="Year", y="Crime Count", color="Sector", barmode="group")
+st.plotly_chart(fig, use_container_width=True)
+
+fig = px.line(dfCrimeCount, x="Year", y="Crime Count", color='Sector')
+st.plotly_chart(fig, use_container_width=True)
---
+++ pages/employment.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+
+import plotly.express as px
+
+
+st.sidebar.header("Employment Data")
