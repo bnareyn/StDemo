89b350d826221b5357b4bbb75487038f04d23121
Binulal Narayanan <binulal@gmail.com>
2022-09-30 13:30:19 -0400

    updated employment viz

--- pages/employment.py
+++ pages/employment.py
@@ -3,5 +3,14 @@ import pandas as pd
 
 import plotly.express as px
 
+DATA_URL = 'https://www.kaggle.com/datasets/pantanjali/unemployment-dataset/download?datasetVersionNumber=1'
 
 st.sidebar.header("Employment Data")
+
+@st.cache
+def get_data():
+    df = pd.read_csv(DATA_URL, encoding = 'latin1')
+    return df
+
+data = get_data()
+data
