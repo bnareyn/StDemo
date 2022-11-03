2fef4f79ee4923633e84af12300b7f27d5d1eb89
Binulal Narayanan <binulal@gmail.com>
2022-11-03 15:41:12 -0400

    new face

--- Home.py
+++ Home.py
@@ -7,6 +7,8 @@ import pandas as pd
 
 import os
 
+import plotly.express as px
+
 hide_streamlit_style = """
 <style>
     #root > div:nth-child(1) > div > div > div > div > section > div {padding-top: 0rem;}
@@ -39,7 +41,7 @@ def setAppConfigs():
     st.set_page_config(
         page_title="DACBoard",
         page_icon="📊",
-        #layout="wide",
+        layout="wide",
         initial_sidebar_state="expanded",
         menu_items={
             'About': "Data Analytics Center - Algonquin College"
@@ -48,7 +50,7 @@ def setAppConfigs():
 
     #st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-    genre = st.radio("",("BYOD", "Kaggle", "DAC"), horizontal = True)
+    #genre = st.radio("",("BYOD", "Kaggle", "DAC"), horizontal = True)
 
     col1, col2, col3, col6 = st.columns(4)
     #with col1:
@@ -62,52 +64,85 @@ def setAppConfigs():
 
     with col6:
         st.image('images/dac.png', width=500)
+
+    #named_colorscales = px.colors.named_colorscales()
+    #color = st.select_slider('Select a color scheme', options = named_colorscales )
+
+
+    df = pd.read_csv('uploaded.csv')
+    id_vars = df.columns[:2]
+    value_vars = df.columns[2:]
     
-    uploaded_file = None
-    if genre == "BYOD":
-        uploaded_file = st.file_uploader("Upload your data")
-    elif genre == "Kaggle":
-        st.write("Kaggle")
-    else:
-        st.write("DAC")
-    
-    from autoviz.AutoViz_Class import AutoViz_Class
-    autoViz = AutoViz_Class()
+    dfMolten = pd.melt(df, id_vars = id_vars, value_vars = value_vars, var_name='Year', value_name='Unemployment')
+    fig = px.choropleth(
+        dfMolten,  
+        locations= dfMolten['Country Code'], 
+        color="Unemployment", 
+        animation_frame=dfMolten["Year"],
+        animation_group=dfMolten["Country Name"],
+        hover_name=dfMolten["Country Name"],
+        color_continuous_scale= 'Portland',
+        labels={ 'Unemployment' : 'Unemployment Rate'}
+    )
 
-    viz = None
-    folderName = "./AutoViz_Plots/AutoViz"
-    fileName = "uploaded.csv"
-    if uploaded_file is not None:
-        dataframe = pd.read_csv(uploaded_file, encoding = 'latin1')
-        #st.write(list(dataframe.columns))
-        dataframe.to_csv(fileName, index = False)
+    fig.update_layout(height = 600, margin={"r":0,"t":0,"l":0,"b":0})
 
-        # cleanup plots folder
-        cleanupFolder( folderName )
+    #fig = go.Figure(data=go.Choropleth(
+    #    locations=df['Country Code'], # Spatial coordinates
+    #    z = df.loc[:, '1991'].astype(float), # Data to be color-coded
+    #    colorscale = 'Blues',
+    #    colorbar_title = "% Unemployment Rate",
+    #))
+    #fig.update_geos(projection_type="natural earth") 
+    st.plotly_chart(fig, use_container_width=True)
 
-        # Create vizualizations
-        avDF = autoViz.AutoViz( fileName, sep=",", 
-        dfte=None, header=0, verbose=0, lowess=False, 
-        chart_format="html", max_rows_analyzed=2000, max_cols_analyzed=20, )
-        print(autoViz.overall)
 
-        # Display visualizations
-        
-        files = os.listdir(path = folderName)
 
-        #for file in files:
-        #    fileName = os.path.join(folderName, file)
-        #    with open( fileName ) as f:
-        #        html = f.read()
-        #        components.html(html, height = 200)
-        tabNames = [ name.split('.')[0].title() for name in files]
-        tabs = st.tabs(tabNames)
-        for tab, file in zip(tabs, files):
-            with tab:
-                fileName = os.path.join(folderName, file)
-                with open( fileName ) as f:
-                    html = f.read()
-                    components.html(html, height = 900)
+    #uploaded_file = None
+    #if genre == "BYOD":
+    #    uploaded_file = st.file_uploader("Upload your data")
+    #elif genre == "Kaggle":
+    #    st.write("Kaggle")
+    #else:
+    #    st.write("DAC")
+    # 
+    #from autoviz.AutoViz_Class import AutoViz_Class
+    #autoViz = AutoViz_Class()
+    # 
+    #viz = None
+    #folderName = "./AutoViz_Plots/AutoViz"
+    #fileName = "uploaded.csv"
+    #if uploaded_file is not None:
+    #    dataframe = pd.read_csv(uploaded_file, encoding = 'latin1')
+    #    #st.write(list(dataframe.columns))
+    #    dataframe.to_csv(fileName, index = False)
+    #
+    #    # cleanup plots folder
+    #    cleanupFolder( folderName )
+    #
+    #    # Create vizualizations
+    #    avDF = autoViz.AutoViz( fileName, sep=",", 
+    #    dfte=None, header=0, verbose=0, lowess=False, 
+    #    chart_format="html", max_rows_analyzed=2000, max_cols_analyzed=20, )
+    #    print(autoViz.overall)
+    #
+    #    # Display visualizations
+    #    
+    #    files = os.listdir(path = folderName)
+    #
+    #    #for file in files:
+    #    #    fileName = os.path.join(folderName, file)
+    #    #    with open( fileName ) as f:
+    #    #        html = f.read()
+    #    #        components.html(html, height = 200)
+    #    tabNames = [ name.split('.')[0].title() for name in files]
+    #    tabs = st.tabs(tabNames)
+    #    for tab, file in zip(tabs, files):
+    #        with tab:
+    #            fileName = os.path.join(folderName, file)
+    #            with open( fileName ) as f:
+    #                html = f.read()
+    #                components.html(html, height = 900)
 
 
 
